607334459b9dc9d43b2f8e4836521933e889b261
Rahunandan K <rahu@apptrion.com>
2017-09-21 21:22:30 +0800

    upgrade parse-server to 2.5.3 and disable live query

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function (req, res) {
-	res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+	res.status(200).send('you have reached Apptrion web site');
 });
 
 // There will be a test page available on the /test path of your server url
@@ -95,9 +95,9 @@ app.get('/test', function (req, res) {
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function () {
-	console.log('parse-server-example running on port ' + port + '.');
+	console.log('parse-server-areevo running on port ' + port + '.');
 });
 
 
 // This will enable the Live Query real-time server
-ParseServer.createLiveQueryServer(httpServer);
+// ParseServer.createLiveQueryServer(httpServer);
